94dbfce494a9b093ef5ad099cdbb432fcc4c8c06
SerhiiMischenko <serj.roshen@gmail.com>
2023-08-15 11:20:26 +0300

    Fix bug with no valid chatId

--- src/main/java/com/example/telegrambotgetcurrencyrate/bot/TelegramBot.java
+++ src/main/java/com/example/telegrambotgetcurrencyrate/bot/TelegramBot.java
@@ -4,15 +4,13 @@ import com.example.telegrambotgetcurrencyrate.configuration.BotConfig;
 import com.example.telegrambotgetcurrencyrate.model.CurrencyModel;
 import com.example.telegrambotgetcurrencyrate.service.CurrencyService;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
@@ -36,81 +34,97 @@ public class TelegramBot extends TelegramLongPollingBot {
         return botConfig.getToken();
     }
 
+    @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
         CurrencyModel currencyModel = new CurrencyModel();
-        String currency = "";
-
-        if (update.hasMessage() && update.getMessage().hasText()) {
+        String currency;
+        if (update.hasMessage()) {
+            System.out.println("In START");
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
-
             if (messageText.equals("/start")) {
+                System.out.println("Chat id massage " +  chatId);
                 startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-            } else if (messageText.equals("Курс валют")) {
-                try {
-                    currency = CurrencyService.getCurrencyRate(messageText, currencyModel);
-                } catch (IOException e) {
-                    sendMessage(chatId, "We have not found such a currency.\n"
-                            + "Enter the currency whose official exchange rate\n"
-                            + "you want to know in relation to BYN.\n"
-                            + "For example: USD");
-                } catch (ParseException e) {
-                    throw new RuntimeException("Unable to parse date");
-                }
-                sendMessage(chatId, currency);
-            } else if (messageText.equals("Новости Украины")) {
-                sendMessage(chatId, "In /news block");
-            } else if (messageText.equals("Погода")) {
-                sendMessage(chatId, "In /weather block");
-            } else {
-                sendMessage(chatId, "Выбери раздел из списка");
             }
         }
-    }
-
-    private void startCommandReceived(Long chatId, String name) {
-            String answer = "Приветствую, " + name + "." + "\n" +
-                    "Я могу предоставить информацию по некоторым темам" + "\n" +
-                    "Выбери интересующую тебя тему";
-            sendMessage(chatId, answer);
+        if (update.hasCallbackQuery()) {
+                System.out.println("In /currency");
+                String callbackData = update.getCallbackQuery().getData();
+            System.out.println(callbackData);
+                if (callbackData.equalsIgnoreCase("/currency")) {
+                    System.out.println("IN");
+                    long chatId = update.getCallbackQuery().getMessage().getChatId();
+                    String messageText = update.getCallbackQuery().getMessage().getText();
+                    currency = CurrencyService.getCurrencyRate(messageText, currencyModel);
+                    System.out.println("Chat id backQuery " +  chatId);
+                        System.out.println(currency);
+                    sendMessage(chatId, currency);
+                } else if (callbackData.equals("/news")) {
+                    long chatId = update.getCallbackQuery().getMessage().getChatId();
+                    sendMessage(chatId, "In /news block");
+                } else if (callbackData.equals("/weather")) {
+                    long chatId = update.getCallbackQuery().getMessage().getChatId();
+                    sendMessage(chatId, "In /weather block");
+                }
+            }
         }
 
-    private void sendMessage(Long chatId, String textToSend) {
-        ReplyKeyboardMarkup replyKeyboardMarkup = createKeyboard();
+    private void startCommandReceived(Long chatId, String name) {
+        String answer = "Приветствую, " + name + "." + "\n" +
+                "Я могу предоставить информацию по некоторым темам" + "\n" +
+                "Выбери интересующую тебя тему";
+        sendStartInlineMessage(chatId, answer);
+    }
+
+    private void sendStartInlineMessage(Long chatId, String textToSend) {
+        InlineKeyboardMarkup inlineKeyboardMarkup = createKeyboard();
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(String.valueOf(chatId));
         sendMessage.setText(textToSend);
 
-        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        sendMessage.setReplyMarkup(inlineKeyboardMarkup);
 
         try {
             execute(sendMessage);
         } catch (TelegramApiException ignored) {
         }
     }
-    private ReplyKeyboardMarkup createKeyboard() {
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-        replyKeyboardMarkup.setResizeKeyboard(true);
-        replyKeyboardMarkup.setOneTimeKeyboard(true);
+    private void sendMessage(Long chatId, String textToSend) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(textToSend);
+        System.out.println("In sendMessage");
+        System.out.println(sendMessage);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException ignored) {
+        }
+    }
+    private InlineKeyboardMarkup createKeyboard() {
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+        List<InlineKeyboardButton> row = new ArrayList<>();
 
-        List<KeyboardRow> keyboardRows = new ArrayList<>();
-        KeyboardRow keyboardRow = new KeyboardRow();
+        InlineKeyboardButton currencyButton = new InlineKeyboardButton();
+        currencyButton.setText("Курс валют");
+        currencyButton.setCallbackData("/currency");
+        row.add(currencyButton);
 
-        // Создание кнопки с текстом "Курс валют" и callback-данными "/currency"
-        KeyboardButton currencyButton = new KeyboardButton("Курс валют");
-        KeyboardButton newsButton = new KeyboardButton("Новости Украины");
-        KeyboardButton weatherButton = new KeyboardButton("Прогноз погоды");
+        InlineKeyboardButton newsButton = new InlineKeyboardButton();
+        newsButton.setText("Новости");
+        newsButton.setCallbackData("/news");
+        row.add(newsButton);
 
-        keyboardRow.add(currencyButton);
-        keyboardRow.add(newsButton);
-        keyboardRow.add(weatherButton);
+        InlineKeyboardButton weatherButton = new InlineKeyboardButton();
+        weatherButton.setText("Погода");
+        weatherButton.setCallbackData("/weather");
+        row.add(weatherButton);
 
-        keyboardRows.add(keyboardRow);
+        keyboard.add(row);
+        inlineKeyboardMarkup.setKeyboard(keyboard);
 
-        replyKeyboardMarkup.setKeyboard(keyboardRows);
-
-        return replyKeyboardMarkup;
+        return inlineKeyboardMarkup;
     }
 
 }
