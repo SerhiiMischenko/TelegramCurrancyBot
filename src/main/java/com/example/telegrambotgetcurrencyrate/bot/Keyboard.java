4ef4b1ab4a8575c4b276da70a93f6c39a27aa252
SerhiiMischenko <serj.roshen@gmail.com>
2023-09-29 10:27:58 +0300

    test

--- src/main/java/com/example/telegrambotgetcurrencyrate/bot/Keyboard.java
+++ src/main/java/com/example/telegrambotgetcurrencyrate/bot/Keyboard.java
@@ -17,7 +17,7 @@ public class Keyboard {
 
     InlineKeyboardMarkup startInlineKeyboard() {
         InlineKeyboardButton currencyButton = new InlineKeyboardButton();
-        currencyButton.setText("ыфвфы");
+        currencyButton.setText("Курс");
         currencyButton.setCallbackData("/currency");
         row.add(currencyButton);
 
