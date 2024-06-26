626031c674979d62d5c2c1871f081b74c9ba6a8d
SerhiiMischenko <serj.roshen@gmail.com>
2024-06-26 23:28:58 +0300

    Fix CurrencyService.java

--- src/main/java/com/example/telegrambotgetcurrencyrate/TelegramBotGetCurrencyRateApplication.java
+++ src/main/java/com/example/telegrambotgetcurrencyrate/TelegramBotGetCurrencyRateApplication.java
@@ -9,5 +9,4 @@ public class TelegramBotGetCurrencyRateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TelegramBotGetCurrencyRateApplication.class, args);
 	}
-	//sout delete
 }
